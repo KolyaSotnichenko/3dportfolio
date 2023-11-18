98af62f45a064ddc86850f6e95865f428e418586
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2023-11-18 19:45:33 +0200

    init app

---
+++ src/constants/index.js
@@ -0,0 +1,235 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    firebase,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nestjs,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nestjs,
+        name: "NestJS",
+        type: "Backend",
+    },
+    {
+        imageUrl: firebase,
+        name: "Firebase",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    // {
+    //     imageUrl: motion,
+    //     name: "Motion",
+    //     type: "Animation",
+    // },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux-toolkit",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Front-End Developer",
+        company_name: "N-Six",
+        icon: summiz,
+        iconBg: "#accbe1",
+        date: "February 2020 - May 2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Front-End Developer",
+        company_name: "LVL99",
+        icon: summiz,
+        iconBg: "#fbc3bc",
+        date: "February 2022 - July 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Front-End Developer",
+        company_name: "SIEMA TECH",
+        icon: summiz,
+        iconBg: "#b7e4c7",
+        date: "April 2023 - August 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Front-End Developer",
+        company_name: "Your Price Booking",
+        icon: summiz,
+        iconBg: "#a2d2ff",
+        date: "October 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/KolyaSotnichenko',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/kolya-sotnichenko/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'DudePlex Movie App',
+        description: 'Streaming movie application.',
+        link: 'https://github.com/KolyaSotnichenko/DudePlex2.0',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'IntelliHire',
+        description: 'Full stack application for prepare young developers to interview.',
+        link: 'https://github.com/KolyaSotnichenko/intellihire-tailwindcss',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-blue',
+        name: 'Genius',
+        description: 'Full stack AI service.',
+        link: 'https://github.com/KolyaSotnichenko/genius',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Luxy',
+        description: 'Full stack task management application.',
+        link: 'https://github.com/KolyaSotnichenko/luxy-project',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'DudePlex mobile',
+        description: 'Mobile application for streaming movies.',
+        link: 'https://github.com/KolyaSotnichenko/DudePlexMobile',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Secure messenger application',
+        description: 'Chat application on React Native also have backend part on github.',
+        link: 'https://github.com/KolyaSotnichenko/SecureMessenger',
+    }
+];
